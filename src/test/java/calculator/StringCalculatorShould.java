74e5cef6458d15f6b67b72998711bf270d6feabd
Magdum Ganesh <magdumganesh4@gmail.com>
2021-10-17 11:49:20 +0530

    Test_addUnknownAmountOfNumber

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -22,5 +22,11 @@ class StringCalculatorShould {
         StringCalculator calculator = new StringCalculator();
         assertEquals(3, calculator.add("1,2"));
     }
+    @Test
+    public void addUnknownAmountOfNumber() throws Exception {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(13, calculator.add("1,2,5,3,2"));
+
+    }
 
 }
